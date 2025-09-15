ae6643d0471adbda4bf4e88ba8bf54cb4f087d0c
damasake3 <171648817+damasake3@users.noreply.github.com>
2025-09-15 20:14:05 +0800

    Init
    - Done Hero
    - Not Mobile responsive yet
    - Focus on emulating Figma Design

---
+++ wwwroot/site.js
@@ -0,0 +1,5 @@
+﻿// wwwroot/js/site.js
+import * as bootstrap from '/lib/bootstrap/dist/js/bootstrap.bundle.min.js';
+
+// Optional: debug to confirm it loads
+console.log("Bootstrap re-imported and ready:", bootstrap);
